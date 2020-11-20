60fcfd90ae41360a328ed701bd4d674e8b9d9b1f
shehan82 <shehansandeepa82@gmail.com>
2020-11-20 22:08:30 +0530

    delete and modified some files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [todo, setTodo] = useState("nothing");
+  const addTodo = () => {
+   
+    let x = document.getElementById("inpt").value;
+    setTodo(x);
+    let k =  <h2>{todo}</h2>;
+    let div = document.getElementById("apnd");
+    div.appendChild(k);
+    
+  };
+
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>TODO Application</h1>
+     <input id="inpt" type="text"/>
+     <button onClick={addTodo}>Add todo</button>
+     <div id="apnd"></div>
     </div>
   );
 }
