a696806d3e5044a8bb5ae66d626c95c22387fa6c
shehan82 <shehansandeepa82@gmail.com>
2020-11-20 22:32:41 +0530

    add some hard coded data to the todo

--- src/App.js
+++ src/App.js
@@ -3,25 +3,21 @@ import React, { useState } from 'react';
 
 
 function App() {
-  const [todo, setTodo] = useState("nothing");
-  const addTodo = () => {
-   
-    let x = document.getElementById("inpt").value;
-    setTodo(x);
-    let k =  <h2>{todo}</h2>;
-    let div = document.getElementById("apnd");
-    div.appendChild(k);
-    
-  };
 
- 
+  const [todos, setTodos] = useState(["hello", "hi", "how", "much"]);
+
+  const add = () => {
+    return todos.map(todo => (
+      <li>{todo}</li>
+    ))
+  }
+
 
   return (
     <div className="App">
-     <h1>TODO Application</h1>
-     <input id="inpt" type="text"/>
-     <button onClick={addTodo}>Add todo</button>
-     <div id="apnd"></div>
+      <ul>
+        {add()}
+      </ul>
     </div>
   );
 }
