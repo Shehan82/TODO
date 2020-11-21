96027c10de97a6f40ae896857fe2ab03952d0937
shehan82 <shehansandeepa82@gmail.com>
2020-11-22 01:51:27 +0530

    add switch for users to change the asc desc order

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import './App.css';
 import React, { useState, useStyles, useEffect } from 'react';
-import { Button, Card, CardContent, Input, InputLabel, TextField } from '@material-ui/core';
+import { Button, Card, CardContent, Input, InputLabel, Switch, TextField } from '@material-ui/core';
 import Todo from './Todo';
 import db from './firebase';
+import firebase from 'firebase';
 
 
 function App() {
 
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
+  const [order, setOrder] = useState("desc");
 
   useEffect(() => {
+    if(order === "desc")
+    {
+      db.collection('todos').orderBy('timestamp','desc').onSnapshot(snapshot => {
+        setTodos(snapshot.docs.map(doc => doc.data().todo))
+      })
+    }
+    if(order === "asc")
+    {
+      db.collection('todos').orderBy('timestamp','asc').onSnapshot(snapshot => {
+        setTodos(snapshot.docs.map(doc => doc.data().todo))
+      })
+    }
+   
 
-    db.collection('todos').onSnapshot(snapshot => {
-      setTodos(snapshot.docs.map(doc => doc.data().todo))
-    })
-
-  }, []) //when this array is in the blank code in the brackets
+  }, [order]) //when this array is in the blank code in the brackets
         // runs ones. and if there is any variable in the array code in the
       // run once at the app load and when the changing happen this variable
       
@@ -30,12 +41,24 @@ function App() {
     event.preventDefault();
 
     db.collection('todos').add({
-      todo:input
+      todo:input,
+      timestamp:firebase.firestore.FieldValue.serverTimestamp(),
     })
     setTodos([...todos, input]);
     setInput("");
     
   };
+
+  const chk = () => {
+    if(order==="desc")
+    {
+      setOrder("asc");
+    }
+    else
+    {
+      setOrder("desc");
+    }
+  }
   return (
     
     <div className="App">
@@ -53,6 +76,12 @@ function App() {
         <TextField  value={input} onChange={chng} id="standard-basic" label="Enter your todo here" />  &nbsp;
         {/* <Input type="text" value={input} onChange={chng} placeholder="Enter your todo here!"/> &nbsp; */}
         <Button disabled={!input} size="large" variant="outlined"  color="primary" type="submit" onClick={addTodo}>add todo</Button>
+        <Switch
+            // checked={()=>{setOrder("asc")}}
+            onChange={chk}
+            // name="checkedB"
+            color="primary"
+          />
        
       </form>
         </CardContent>
